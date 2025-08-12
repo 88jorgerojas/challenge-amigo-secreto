39becafd483e999673843d6cc036ee6b4e8cab15
88jorgerojas <jorgeh.rojas_88@outlook.com>
2025-08-12 16:59:14 -0500

    implementando la primera parte de la logica

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//variables globales 
+let listaAmigos = [];
+//funiones complementarias
+function mostrarListaDeAmigos(){
+
+
+}
+
+// funsiones principales 
+function agregarAmigo(){
+    let obtenerNombres = document.getElementById("amigo").value;
+    listaAmigos.push(obtenerNombres);
+
+    
+    let mostrandoNombres = document.getElementById("listaAmigos");
+    mostrandoNombres.innerHTML=`<li>${listaAmigos}</li>`;
+
+}
+
+function sortearAmigo(){
+
+
+
+}
