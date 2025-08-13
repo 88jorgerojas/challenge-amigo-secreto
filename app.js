f83529b30dc8444e706f6ac2e8f4ffdc05e6c862
88jorgerojas <jorgeh.rojas_88@outlook.com>
2025-08-12 23:13:57 -0500

    ajustes  del imput, implementando el metodo .join  nombres organizados con su respectivo espacio

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//variables globales 
+let listaAmigos = [];
+//funiones complementarias
+function mostrarListaDeAmigos(){
+
+
+}
+
+// funsiones principales 
+function agregarAmigo(){
+    let obtenerNombres = document.getElementById("amigo").value;
+// evita agregar nombres vacios
+    if(obtenerNombres.trim() === ""){
+        alert("Por favor ingresa un nombre válido.");
+        return;
+    }
+    listaAmigos.push(obtenerNombres);
+//resetear el imput cada vez que se ingrese un nombre, para ingresar uno nuevo
+    document.getElementById("amigo").value = "";
+//mostrar los nombres en forma de colunma, no en una sola linea 
+    let mostrandoNombres = document.getElementById("listaAmigos");
+    mostrandoNombres.innerHTML=`<li>${listaAmigos.join("</li><li>")}</li>`;
+
+}
+
+function sortearAmigo(){
+
+
+
+}
